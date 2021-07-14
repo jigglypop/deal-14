74b187f4839f20f96dc3bc9df3993af9a82f3d3d
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-07-14 11:00:20 +0900

    fix: init query를 서버 구동 시 실행

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express';
-import initialize from '../controllers/init.controller';
 import authRouter from './auth';
 import productRouter from './product'
 import townRouter from "./town"
 
 const apiRouter = Router();
 
-apiRouter.use('/', initialize)
 apiRouter.use('/auth', authRouter);
 apiRouter.use('/product', productRouter);
 apiRouter.use('/town', townRouter);
