01237453a62cd2fe406149b235ab4db57d648bfc
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-07-14 19:30:15 +0900

    add: 좋아요 API 복구

--- backend/src/routes/product/index.ts
+++ backend/src/routes/product/index.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import likedProductController from '../../controllers/liked-product.controller';
 import productController from '../../controllers/product.controller';
 import authMiddleware from '../../middlewares/auth.middleware';
 import wrapAsync from '../../middlewares/wrap-async';
@@ -7,5 +8,7 @@ const productRouter = Router();
 
 productRouter.post('/', authMiddleware, wrapAsync(productController.write));
 productRouter.get('/', authMiddleware, wrapAsync(productController.readDetails));
+productRouter.post('/like/:productId', authMiddleware, wrapAsync(likedProductController.like));
+productRouter.post('/unlike/:productId', authMiddleware, wrapAsync(likedProductController.unlike));
 
 export default productRouter;
