e8ebe78a8ffdafb2117de33ea181c6f4eb6410ae
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-07-14 21:02:07 +0900

    fix: 전체/개별 상품 조회 API accessAuthMiddleware로 변경

--- backend/src/routes/product/index.ts
+++ backend/src/routes/product/index.ts
@@ -1,16 +1,17 @@
 import { Router } from 'express';
 import likedProductController from '../../controllers/liked-product.controller';
 import productController from '../../controllers/product.controller';
+import accessAuthMiddleware from '../../middlewares/access-auth.middleware';
 import authMiddleware from '../../middlewares/auth.middleware';
 import wrapAsync from '../../middlewares/wrap-async';
 
 const productRouter = Router();
 
-productRouter.get('/', authMiddleware, wrapAsync(productController.readDetails));
+productRouter.get('/', accessAuthMiddleware, wrapAsync(productController.readDetails));
 productRouter.get('/liked', authMiddleware, wrapAsync(productController.readLikedDetails));
 productRouter.post('/', authMiddleware, wrapAsync(productController.write));
 
-productRouter.get('/:productId', authMiddleware, wrapAsync(productController.readDetail));
+productRouter.get('/:productId', accessAuthMiddleware, wrapAsync(productController.readDetail));
 
 productRouter.post('/like/:productId', authMiddleware, wrapAsync(likedProductController.like));
 productRouter.post('/unlike/:productId', authMiddleware, wrapAsync(likedProductController.unlike));
